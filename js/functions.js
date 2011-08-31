f6117c648e74fbcd6f7d065850d79aa2dc6728ad
Nidhi Krishen <nidhi@solutionmedia.net>
2011-08-31 15:38:10 -0400

    Commented out code that automatically loads topics upon page initialization

--- js/functions.js
+++ js/functions.js
@@ -32,13 +32,14 @@ $(document).ready(function() {
 
 	$.data($("body").get(0), "orgSelect", $("#orgs").html());
 	$("#orgs").val("CHE");
-	$("#orgs").change(_.throttle(function() { chgSelects("topic"); }, 400));
-	$("#topics").change(_.throttle(function() { chgSelects("org"); }, 400));
-	$("#primary_topic").change(_.throttle(function() { chgSelects("org"); }, 400));
-	$("#leftOption3 select").change(_.throttle(function() { chgSelects("all"); }, 400)); // Make sure you don't overload Javascript!
-	chgSelects("topic");
+	 //NK - comment below out if you do not want to initialize the page showing the topics for some preselected items
+	//$("#orgs").change(_.throttle(function() { chgSelects("topic"); }, 400));
+	//$("#topics").change(_.throttle(function() { chgSelects("org"); }, 400));
+	//$("#primary_topic").change(_.throttle(function() { chgSelects("org"); }, 400));
+	//$("#leftOption3 select").change(_.throttle(function() { chgSelects("all"); }, 400)); // Make sure you don't overload Javascript!
+	//chgSelects("topic");
 
-	submitMenu();
+	//submitMenu();
 
 	// Tabs
 
